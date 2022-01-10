d479ec1d859b88cd96daee7d1da9480697a8b316
lsdpirate <nickload22@live.com>
2022-01-10 21:03:59 +0100

    Made the function return precision and recall for each class

--- satimage/compute_f_scores.R
+++ satimage/compute_f_scores.R
@@ -1,6 +1,8 @@
 compute_f_scores = function(labels, confusion_matrix, weights){
   fscores = list()
   fscores.weighted = list()
+  recalls = list()
+  precisions = list()
   for (cls in labels){
     TP = confusion_matrix[cls, cls]
     precision = TP/sum(confusion_matrix[cls,])
@@ -9,7 +11,9 @@ compute_f_scores = function(labels, confusion_matrix, weights){
     f_score.weighted = f_score * weights[cls]
     fscores[[cls]] = f_score
     fscores.weighted[[cls]] = f_score.weighted
+    recalls[[cls]] = recall
+    precisions[[cls]] = precision
   }
-  ret_val = list("fscores" = fscores, "fscores.weighted" = fscores.weighted)
+  ret_val = list("fscores" = fscores, "fscores.weighted" = fscores.weighted, "recalls" = recalls, "precisions" = precisions)
   return(ret_val)
 }
